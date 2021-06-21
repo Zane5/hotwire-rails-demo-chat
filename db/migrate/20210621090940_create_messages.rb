ba7601ebf1aa3a881fd5ca31ffb4811eb14bee55
vagrant <vagrant@x.dev>
2021-06-21 09:11:37 +0000

    fix

--- db/migrate/20210621090940_create_messages.rb
+++ db/migrate/20210621090940_create_messages.rb
@@ -1,7 +1,7 @@
 class CreateMessages < ActiveRecord::Migration[6.1]
   def change
     create_table :messages do |t|
-      t.reference :room
+      t.references :room
       t.text :content
 
       t.timestamps
